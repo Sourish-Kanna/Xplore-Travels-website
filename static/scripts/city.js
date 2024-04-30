605d9bb804f34cb58865e5e691553ce81d84c15b
Sourish Kanna <sourishkanna001@gmail.com>
2024-04-30 11:03:28 +0530

    end monent change

--- static/scripts/city.js
+++ static/scripts/city.js
@@ -78,5 +78,5 @@ function showSlides() {
     slideIndex++;
     if (slideIndex > slides.length) { slideIndex = 1 }
     slides[slideIndex - 1].style.display = "block";
-    setTimeout(showSlides, 4000); // Change image every 4 seconds
+    setTimeout(showSlides, 2000); // Change image every 4 seconds
 }
