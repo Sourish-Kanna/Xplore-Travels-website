930fc2b3e2d41207d11b25d0d9e419b419898e41
Sourish Kanna <115524993+Sourish-Kanna@users.noreply.github.com>
2024-06-02 01:11:57 +0530

    Updated app.py for local run

--- app.py
+++ app.py
@@ -1,5 +1,4 @@
 from flask import Flask, render_template
-import webbrowser 
 import json
 import dbms
 import re
@@ -37,5 +36,6 @@ def other(click):
     return render_template(template_name_or_list=f"{click}.html")
 
 if __name__ == '__main__':  
+   import webbrowser
    webbrowser.open("http://127.0.0.1:5000/")
    app.run()
